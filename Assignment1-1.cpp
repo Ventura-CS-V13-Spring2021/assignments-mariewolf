86ca5635f13f92b17674b3026f2095f86ee31646
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-02-09 19:15:38 +0000

    edited assignment1-1

--- Assignment1-1.cpp
+++ Assignment1-1.cpp
@@ -5,26 +5,29 @@ using namespace std;
 int main()
 {
                int aClass, bClass, cClass;
+               double totalA, totalB, totalC, total_amount;
                
                const double rateA = 15.00;
                const double rateB = 12.00;
                const double rateC = 9.00;
                
-               cout << "How many class A, B, & C seats were sold?\nA:";
+               cout << "How many class A, B, & C seats were sold?\nA: ";
                cin >> aClass;
-               cout << "B:";
+               cout << "B: ";
                cin >> bClass;
-               cout << "C:";
+               cout << "C: ";
                cin >> cClass;
                
-               double totalA = aClass * rateA;
-               double totalB = bClass * rateB;
-               double totalC = cClass * rateC;
-               double total_amount = totalA + totalB + totalC;
+               totalA = aClass * rateA;
+               totalB = bClass * rateB;
+               totalC = cClass * rateC;
+               total_amount = totalA + totalB + totalC;
                
-               // print total_amount, totalA, totalB, totalC
-               cout << "The grand total for sales is $" << total_amount;
+               cout << fixed << showpoint << setprecision(2);
+               cout << "\nThe grand total for sales is $" << total_amount;
                cout << "\nClass A sales: $" << totalA;
                cout << "\nClass B sales: $" << totalB;
                cout << "\nClass C sales: $" << totalC;
+               
+               return 0;
 }
