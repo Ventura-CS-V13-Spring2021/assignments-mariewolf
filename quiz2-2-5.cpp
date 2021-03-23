9e0085eceac9f62b2e927402f7f1d6700f5f912e
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-03-23 04:28:42 +0000

    edited quiz2-2-5.cpp

--- quiz2-2-5.cpp
+++ quiz2-2-5.cpp
@@ -3,17 +3,17 @@ using namespace std;
 int main()
 {
   int range1, range2;
+  bool flag = false;
   cout << "Enter two integer values: ";
   cin >> range1 >> range2;
   if (range1 >= range2)
     cout << "\nInput error: First input must be less than the second.";
+  cout << "\n";
   for (int i=range1; i<=range2; i++) {
-    for ()
+    for (int j = 2; j<i; j++) {
+      if (i % j == 0) {break;}
+      else {cout << i << " ";}
+    }
   }
-
-
-
-
-
-    return 0;
+  return 0;
 }
