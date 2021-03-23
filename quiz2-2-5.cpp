523fdd1d0e3d02665c9b4dd74fb29f96d8daf3a5
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-03-23 04:23:32 +0000

    edited quiz2-2-5.cpp

--- quiz2-2-5.cpp
+++ quiz2-2-5.cpp
@@ -7,4 +7,13 @@ int main()
   cin >> range1 >> range2;
   if (range1 >= range2)
     cout << "\nInput error: First input must be less than the second.";
+  for (int i=range1; i<=range2; i++) {
+    for ()
+  }
+
+
+
+
+
+    return 0;
 }
