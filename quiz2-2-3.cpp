adc59478e0a42bac807dfba6f7a52bfc40b570e5
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-03-23 04:06:48 +0000

    edited quiz2-2-3.cpp

--- quiz2-2-3.cpp
+++ quiz2-2-3.cpp
@@ -9,6 +9,13 @@ int main()
   if ((number2 < 0) || (number2 > 99)) flag = 1;
   if ((number3 < 0) || (number3 > 99)) flag = 1;
   if (flag == 1) {
-    cout << "Input error: Not in range.";
+    cout << "\nInput error: Not in range.";
+    main();
   }
+  if ((number1 == number2) || (number1 == number3))
+    cout << "Number " << number1 << "is repeated.";
+  if (number2 == number3)
+    cout << "Number " << number2 << "is repeated.";
+  else
+    cout << "There is no duplicated number.";
 }
