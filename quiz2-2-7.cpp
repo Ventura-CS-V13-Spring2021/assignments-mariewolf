3dc02fa525e1ae44a3241b579bb687d3ce6b80d9
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-03-23 05:12:20 +0000

    edited quiz2-2-7.cpp

--- quiz2-2-7.cpp
+++ quiz2-2-7.cpp
@@ -4,15 +4,20 @@
 using namespace std;
 int main()
 {
+  int empID, N;
+  string empName, depName;
+  double salary;
   ofstream ofs("employee.txt");
 
   cout << "Enter how many employees you have: ";
   cin >> N;
 
   for (int i = 1; i <= N; i++){
-    cout >> "Enter the employee's ID, name, department, and salary: ";
-    cin >> empID, empName, depName, salary;
+    cout << "Enter the employee's ID, name, department, and salary: ";
+    cin >> empID >> empName >> depName >> salary;
+    ofs << empID << empName << depName << salary;
   }
 
   ofs.close();
+  return 0;
 } 
