61a296c6a66fa9a0af11a1d00ab38b3d1b284efc
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-03-23 04:35:25 +0000

    edited quiz2-2-5.cpp

--- quiz2-2-5.cpp
+++ quiz2-2-5.cpp
@@ -6,13 +6,16 @@ int main()
   bool flag = false;
   cout << "Enter two integer values: ";
   cin >> range1 >> range2;
+  cout << "\nThe prime numbers in the range are: ";
   if (range1 >= range2)
     cout << "\nInput error: First input must be less than the second.";
-  cout << "\n";
   for (int i=range1; i<=range2; i++) {
     for (int j = 2; j<i; j++) {
-      if (i % j == 0) {break;}
-      else {cout << i << " ";}
+      if (i % j == 0) {continue;}
+      else {
+        cout << i << " ";
+        break;
+        }
     }
   }
   return 0;
