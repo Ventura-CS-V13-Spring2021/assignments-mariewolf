cfa56c6a885cec92026f497de03fd04b8735f410
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-03-23 05:39:28 +0000

    edited quiz2-2-8.cpp

--- quiz2-2-8.cpp
+++ quiz2-2-8.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 int main()
 {
-  int empID, total, counter = 0;
+  int empID, total, counter, N = 0;
   string empName, depName;
   double salary;
   ifstream empfile;
   empfile.open("employee.txt");
+  cout << "ID     Name    Department    Salary\n";
   while (!empfile.fail()) {
     empfile >> empID >> empName >> depName >> salary;
-    cout << empID << " " << empName << " " << depName << " " << salary << endl;
+    cout << empID << "   " << empName << "   " << depName << "   " << salary << endl;
     total += salary;
     counter += 1;
   }
   cout << "The total of the salaries is: " << total << endl;
   cout << "The average of the salaries is: " << total/double(counter);
+  empfile.close();
   return 0;
 } 
