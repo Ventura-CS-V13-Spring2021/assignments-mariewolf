cd2f78bddd72fbefcb0794cb6ed77abc5b209bfd
mariewolf1 <sarah_wolfel1@my.vcccd.edu>
2021-03-23 05:22:57 +0000

    edited quiz2-2-8.cpp

---
+++ quiz2-2-8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+int main()
+{
+  int empID;
+  int N = 0;
+  string empName, depName;
+  double salary;
+  ifstream empfile;
+  empfile.open("employee.txt");
+  while (empfile >> N) {
+
+  }
+  return 0;
+} 
