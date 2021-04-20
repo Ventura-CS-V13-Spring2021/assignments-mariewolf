2ef96ac7972c3af96ea8451e84b3b0326ea88564
mariewolf1 <>
2021-04-20 03:38:44 +0000

    edited q-1

--- MIDTERM/q-1.cpp
+++ MIDTERM/q-1.cpp
@@ -4,17 +4,22 @@ using namespace std;
 
 int main()
 {
-  ifstream inStream("students.txt");
+  ifstream inStream("MIDTERM/students.txt");
   string studentName;
-  int score1, score2, counter = 0;
+  int score1, score2;
+  int counter = 0, gradeCounter = 0;
+  double total;
 
-  for (int i=1; i<=20; i++)
+  for (int i=1; i<=10; i++)
   {
     if (counter <= 3) {
       inStream >> studentName >> score1 >> score2;
       cout << "Student name: " << studentName << endl;
-      cout << "Score 1: " << score1 << "Score 2: " << score2 << endl;
+      cout << "Score 1: " << score1 << "  Score 2: " << score2 << endl;
+      total = score1+score2;
+      cout << "Sum: " << total << "      Average: " << total/2.0 << endl;
       counter++;
+      cout << endl;
     }
     if (counter = 3)
       counter = 0;
