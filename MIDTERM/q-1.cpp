861a4db9462b2909f9f31a8d8aa1c015d197747a
mariewolf1 <>
2021-04-20 03:47:37 +0000

    finished q-1

--- MIDTERM/q-1.cpp
+++ MIDTERM/q-1.cpp
@@ -8,7 +8,7 @@ int main()
   string studentName;
   int score1, score2;
   int counter = 0, gradeCounter = 0;
-  double total;
+  double total, average;
 
   for (int i=1; i<=10; i++)
   {
@@ -17,14 +17,18 @@ int main()
       cout << "Student name: " << studentName << endl;
       cout << "Score 1: " << score1 << "  Score 2: " << score2 << endl;
       total = score1+score2;
-      cout << "Sum: " << total << "      Average: " << total/2.0 << endl;
+      average = total/2.0;
+      cout << "Sum: " << total << "      Average: " << average << endl;
       counter++;
       cout << endl;
+      if (average > 80)
+        gradeCounter++;
     }
     if (counter = 3)
       counter = 0;
   }
 
+  cout << "The total number of students who have an average > 80: " << gradeCounter << endl;
   return 0;
 
 }
