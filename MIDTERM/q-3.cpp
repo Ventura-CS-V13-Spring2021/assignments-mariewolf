23c5c72267314790485c1e77c7592c758775eef6
mariewolf1 <>
2021-04-20 04:54:45 +0000

    edited q-3

--- MIDTERM/q-3.cpp
+++ MIDTERM/q-3.cpp
@@ -11,8 +11,32 @@ int fileWrite(int difference);
 
 int main()
 {
+  int num1 = getRdnum();
+  int num2 = getRdnum();
+  int num3 = getRdnum();
 
+
+
+  return 0;
 }
+
+int findMin(int num1, int num2, int num3) {
+  //insert code here
+}
+
+int findMax(int num1, int num2, int num3) {
+  //insert code here
+}
+
+int getDifference(int num1, int num2, int num3) {
+  //insert code here
+}
+
+int getRdnum(void){
+  int num = rand() % 9 + 1;
+  return num;
+}
+
 
 int fileWrite(int difference) {
   ofstream outStream("MIDTERM/q3numbers.txt", ios::app);
@@ -20,4 +44,8 @@ int fileWrite(int difference) {
   if (outStream.fail()) {
     cout << "Output file opening failed.\n";
   }
+
+  outStream << difference << endl;
+
+  outStream.close();
 }
