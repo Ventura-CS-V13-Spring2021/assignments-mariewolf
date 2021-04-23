ad591b090aceae684ff384056a48bb8050088e4b
mariewolf1 <>
2021-04-23 04:53:12 +0000

    edited q-1; midterm extension

--- MIDTERM/q-1.cpp
+++ MIDTERM/q-1.cpp
@@ -4,32 +4,29 @@ using namespace std;
 
 int main()
 {
-  ifstream inStream("MIDTERM/students.txt");
+  ifstream inStream("students.txt");
+  if (inStream.fail()) {
+    cout << "Input file opening failed.\n";
+  }
   string studentName;
-  int score1, score2;
-  int counter = 0, gradeCounter = 0;
+  int score1, score2, gradeCounter = 0;
   double total, average;
 
   for (int i=1; i<=10; i++)
   {
-    if (counter <= 3) {
-      inStream >> studentName >> score1 >> score2;
-      cout << "Student name: " << studentName << endl;
-      cout << "Score 1: " << score1 << "  Score 2: " << score2 << endl;
-      total = score1+score2;
-      average = total/2.0;
-      cout << "Sum: " << total << "      Average: " << average << endl;
-      counter++;
-      cout << endl;
-      if (average > 80)
-        gradeCounter++;
-    }
-    if (counter = 3)
-      counter = 0;
+    inStream >> studentName >> score1 >> score2;
+    cout << "Student name: " << studentName << endl;
+    cout << "Score 1: " << score1 << "  Score 2: " << score2 << endl;
+
+    total = score1+score2;
+    average = total/2.0;
+    cout << "Sum: " << total << "      Average: " << average << endl << endl;
+
+    if (average > 80)
+      gradeCounter++;
   }
 
   cout << "The total number of students who have an average > 80: " << gradeCounter << endl;
   inStream.close();
   return 0;
-
 }
