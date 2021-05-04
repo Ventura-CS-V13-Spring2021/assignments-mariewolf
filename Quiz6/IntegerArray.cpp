7f58dd5e45de4d54d5e3341e30565576f54bd182
mariewolf1 <>
2021-05-04 02:55:53 +0000

    edited quiz6

---
+++ Quiz6/IntegerArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include "IntegerArray.hpp"
+using namespace std;
+
+int getLength(void) const {
+  //retrieve length
+}
+void sortArray(int flag) {
+  //sort array
+}
+void fillUp(void); {
+  //fill array
+}
+void getPrimenumber(void) const {
+  //count # of primes
+}
+void printAll(void) const {
+  //print array
+}
--- Quiz6/q-1.cpp
+++ Quiz6/q-1.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include "IntegerArray.hpp"
 using namespace std;
 
+int main()
+{
+  IntegerArray N;
 
+  N.fillUp();
+  N.printAll();
+  cout << "Current length is " << N.getLength() << endl;
 
-int main() {
-  //insert code here
+  cout << "After sorting with ascending order : \n ";
+  N.sortArray(0);
+  N.printAll();
+
+  cout << "After sorting with descending order: \n ";
+  N.sortArray(1);
+  N.printAll();
+
+  cout << "The number of prime numbers " << N.getPrimenumbers() << endl;
 }
+
+//code copied from question prompt
