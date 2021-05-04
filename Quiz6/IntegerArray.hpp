05e24edc1efd4d7aa285e003db8a5687810752b8
mariewolf1 <>
2021-05-04 02:48:18 +0000

    started quiz6

---
+++ Quiz6/IntegerArray.hpp
@@ -0,0 +1,17 @@
+#ifndef INTEGERARRAY_H
+#define INTEGERARRAY_H
+class IntegerArray
+{
+private:
+    static const int N = 10;
+    int numbers[N];
+    int length = 0;
+
+public:
+    int getLength(void) const;
+    void sortArray(int flag);
+    void fillUp(void);
+    void getPrimenumber(void) const;
+    void printAll(void) const;
+};
+#endif
