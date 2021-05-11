39198d6e8dc30646cbda15d914617dc01ea6c8b2
mariewolf1 <>
2021-05-11 03:33:48 +0000

    edited quiz 7

--- Quiz7/coordinate.cpp
+++ Quiz7/coordinate.cpp
@@ -4,9 +4,21 @@
 /*     double x;
     double y; */
 
-Coordinate()
-Coordinate(double xval, double yval)
-int getX() const
-int getY() const
-void setXY(double xval, double yval)
-void printXY() const
+Coordinate::Coordinate() : x(0), y(0) {
+  //do nothing
+}
+Coordinate::Coordinate(double xval, double yval) : x(xval), y(yval) {
+  //do nothing
+}
+int Coordinate::getX() const {
+  //do something
+}
+int Coordinate::getY() const {
+  //do something
+}
+void Coordinate::setXY(double xval, double yval) {
+  //do something
+}
+void Coordinate::printXY() const {
+  //do something
+}
--- Quiz7/rectangle.cpp
+++ Quiz7/rectangle.cpp
@@ -7,11 +7,27 @@ Coordinate rt;
 Coordinate center;
 double area;*/
 
-Rectangle()
-Rectangle(Coordinate lbval, Coordinate rtval)
-Coordinate getLB() const
-Coordinate getRT() const
-double getArea()
-Coordinate getCenter()
-void setLBRT(Coordinate lbval, Coordinate rtval)
-void printRectangle() const
+Rectangle::Rectangle() : lb(0), rt(0) {
+  //do nothing
+}
+Rectangle::Rectangle(Coordinate lbval, Coordinate rtval) : lb(lbval), rt(rtval) {
+  //do nothing
+}
+Coordinate Rectangle::getLB() const {
+  //do something
+}
+Coordinate Rectangle::getRT() const {
+  //do something
+}
+double Rectangle::getArea() {
+  //do something
+}
+Coordinate Rectangle::getCenter() {
+  //do something
+}
+void Rectangle::setLBRT(Coordinate lbval, Coordinate rtval) {
+  //do something
+}
+void Rectangle::printRectangle() const {
+  //do something
+}
