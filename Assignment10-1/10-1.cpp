1dfb448436cdb7e514d9f08644a0016c750bd653
mariewolf1 <>
2021-05-13 22:12:41 +0000

    edited assignment 10-1

---
+++ Assignment10-1/10-1.cpp
@@ -0,0 +1,100 @@
+#include <string>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+
+class Student
+{
+private:
+  string name;
+  int numClasses;
+  string *classList;
+
+public:
+  Student() : name("Blank"), numClasses(0), classList(NULL){};
+  Student(string n, int num);
+  Student(Student &rhs);
+  Student &operator=(Student &rhs);
+  ~Student();
+  void inputClass(string n, int num);
+  void resetClass();
+  void printAll() const;
+  string getName() const {return name;};
+  int getNumclasses() const {return numClasses;};
+};
+
+Student::Student(string n, int num) {
+  name = n;
+  numClasses = num;
+  classList = new string[num];
+}
+Student::Student(Student &rhs) {
+  //deep copy
+  name = rhs.name;
+  numClasses = rhs.numClasses;
+  classList = new string[numClasses];
+  for (int i=0; i<numClasses; i++)
+    classList[i] = rhs.classList[i];
+}
+Student::~Student() {
+  //destructor
+  name = " ";
+  numClasses = 0;
+  delete [] classList; //valid
+}
+Student &Student::operator=(Student &rhs) {
+  // = operator
+  name = rhs.name;
+  numClasses = rhs.numClasses;
+  classList = new string[numClasses];
+  for (int i=0; i<numClasses; i++)
+    classList[i] = rhs.classList[i];
+}
+void Student::inputClass(string n, int num) {
+  //gathers input for classes
+  classList = new string[num];
+  for (int i=0; i<num; i++) {
+    cout << "Enter class #" << i+1 << ": ";
+    cin >> classList[i];
+  }
+}
+void Student::resetClass() {
+  //resets class
+  name = " ";
+  numClasses = 0;
+  memset(classList, 0, sizeof(classList));
+}
+void Student::printAll() const {
+  //print all student info
+  cout << "\nStudent \"" << getName() << "\" has " << getNumclasses() << " classes. They are:" << endl;
+  for (int i=0; i<getNumclasses(); i++) {
+    cout << classList[i] << endl;
+  }
+}
+
+int main() {
+  int num;
+  string sname;
+  cout << "Input the student's name: ";
+  cin >> sname;
+  cout << "Input the number of classes: ";
+  cin >> num;
+  Student sA(sname, num);
+  sA.inputClass(sname, num);
+
+  cout << "\nThis is a print of that student.";
+  sA.printAll();
+
+  Student sB = sA;
+  cout << "\nThis is a copied object of that student.";
+  sB.printAll();
+
+  cout << "\nReturned name: " << sB.getName() << endl;
+  cout << "Returned number of classes: " << sB.getNumclasses() << endl;
+
+  cout << "\nThis method resets the information for the second student object.";
+  sB.resetClass();
+  sB.printAll();
+  return 0;
+}
--- Assignment10-1/assignment10-1.cpp
+++ Assignment10-1/assignment10-1.cpp
@@ -1,38 +1,65 @@
 #include <string>
 #include <iostream>
+//#include <algorithm>
+//#include <cstring>
 using namespace std;
 
 class Student
 {
 private:
   string name;
-  int num_classes;
-  string *class_list;
+  int numClasses;
+  string *classList;
 
 public:
-  Student() : name("Test"), num_classes(0), class_list(NULL){};
-  Student(string n, int num) : name(n), num_classes(num);
+  Student() : name("Blank"), numClasses(0), classList(NULL){};
+  Student(string n, int num) : name(n), numClasses(num) {};
   Student(Student &rhs);
-  ~Student();
   Student &operator=(Student &rhs);
+  ~Student();
   void inputClass(string n, int num);
   void resetClass();
   void printAll() const;
-  string getName() const {return name};
-  int getNumclasses() const {return num_classes};
+  string getName() const {return name;};
+  int getNumclasses() const {return numClasses;};
 };
 
-Student::Student(string n, int num) {
-}
 Student::Student(Student &rhs){
+  //deep copy
+  //new name = rhs.name;
+  //new numClasses = rhs.numClasses;
+  //new classList = rhs.classList;
+}
+Student::~Student() {
+  //deconstructor
+  delete &name;
+  delete &numClasses;
+  delete [] classList;
 }
 Student &Student::operator=(Student &rhs) {
+  // = operator
+  name = rhs.name;
+  numClasses = rhs.numClasses;
+  classList = rhs.classList;
 }
 void Student::inputClass(string n, int num) {
+  //gathers input for classes
+  classList = new string[num];
+  for (int i=0; i<num; i++) {
+    cout << "Enter class #" << i+1 << ": ";
+    cin >> classList[i];
+  }
 }
 void Student::resetClass() {
+  //reset class
 }
 void Student::printAll() const {
+  //print all student info
+  cout << "\nStudent " << getName() << " has " << getNumclasses() << " classes.";
+  cout << " They are: " << endl;
+  for (int i=0; i<getNumclasses(), i++) {
+    cout << classList[i] << endl;
+  }
 }
 
 int main() {
@@ -43,5 +70,8 @@ int main() {
   cout << "Input the number of classes: ";
   cin >> num;
   Student s(sname, num);
+  s.inputClass(sname, num);
+  s.printAll();
+
 
 }
