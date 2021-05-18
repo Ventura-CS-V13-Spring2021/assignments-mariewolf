f4e35554d7665c68ab6bdcfbe8a0f1a1c7dccdf0
mariewolf1 <>
2021-05-18 18:25:27 +0000

    edited q-2

--- Final/q-2.cpp
+++ Final/q-2.cpp
@@ -6,17 +6,34 @@ int countRepeats(int [], int);
 void printArr(int [], int);
 
 int main() {
-  int N = 10;
-  int arr[N] = {3,5,16,25,26,30};
-  sort(arr, arr+N);
+  unsigned seed = time(0); //random number seed
+  srand(seed);
+  int N = rand() % 20;
+  int arr[N];
+
+  for (int i=0; i<N; i++){
+    arr[i] = (rand() % 20) - 5;
+  }
+
+  sort(arr, arr+N, greater<int>());;
   printArr(arr, N);
+
 
   return 0;
 }
 
 void printArr(int arr[], int N) {
+  cout << "Printing array: " << endl;
   for (int i=0; i<N; i++) {
     cout << arr[i] << " ";
+  }
+
+  int prevElm = 0;
+
+  cout << "\nDistinct elements: ";
+  for (int i=0; i<N; i++) {
+    if (prevElm != arr[i])
+      cout << arr[i] << " ";
   }
 }
 
