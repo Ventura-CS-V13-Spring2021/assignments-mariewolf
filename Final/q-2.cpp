009297ef6d58e5057c06f2c105828aa823f5264a
mariewolf1 <>
2021-05-18 18:15:41 +0000

    edited q-2

--- Final/q-2.cpp
+++ Final/q-2.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int countRepeats(int [], int);
+void printArr(int [], int);
 
 int main() {
   int N = 10;
   int arr[N] = {3,5,16,25,26,30};
-  int num = getNumDiv(arr, N);
-  cout << "The number with the most factors is: " << num << endl;
+  sort(arr, arr+N);
+  printArr(arr, N);
 
   return 0;
 }
 
-int countRepeats(int numbers[], N) {
+void printArr(int arr[], int N) {
+  for (int i=0; i<N; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+int countRepeats(int numbers[], int N) {
   //count # of repeats
 
 }
